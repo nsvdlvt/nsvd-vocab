980f4cb3ea3cc26ec4b5d79e95bc0f48713cae37
Dung Nguyen <nsvd.lvt@gmail.com>
2026-05-11 14:41:43 +0700

    2h40 11th5

---
+++ app/(dashboard)/folders/page.tsx
@@ -0,0 +1,9 @@
+export default function FoldersPage() {
+  return (
+    <section className="p-8">
+      <h1 className="text-5xl font-black">
+        Kho lưu trữ
+      </h1>
+    </section>
+  )
+}
